9b82665cec4b96f289e77944089e256d665f5db8
Conrad Savio Jude Gomes <conrad.s.j.gomes@gmail.com>
2015-12-17 09:19:08 +0530

    Initial commit of current code excercises
    Signed-off-by: Conrad Savio Jude Gomes <conrad.s.j.gomes@gmail.com>

---
+++ chapter1/1.1.c
@@ -0,0 +1,11 @@
+/* Exercise 1-1. Run the "hello, world" program on your system. Experiment
+ * with leaving out parts of the program, to see what error messages you get. */
+
+#include <stdio.h>
+
+main ()
+{
+	printf("hello, ");
+	printf("world");
+	printf("\n");
+}
---
+++ chapter1/1.3.c
@@ -0,0 +1,18 @@
+/* Exercise 1-3. Modify the temperature conversion program to print a heading
+ * above the table. */
+
+#include <stdio.h>
+
+#define LOWER 0		/* lower limit of the table */
+#define UPPER 300	/* upper limit of the table */
+#define STEP  20	/* step size */
+
+/* pring Farenheit-Celsius table
+ * for fahr = 0, 20, ..., 300 */
+main ()
+{
+	int fahr;
+
+	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) 
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
+}
