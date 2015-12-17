f60d5c0ac6929976b01addd52d505c3353cae272
Conrad Savio Jude Gomes <conrad.s.j.gomes@gmail.com>
2015-12-17 09:28:20 +0530

    Changed to print in reverse order
    Signed-off-by: Conrad Savio Jude Gomes <conrad.s.j.gomes@gmail.com>

--- chapter1/1.5.c
+++ chapter1/1.5.c
@@ -1,5 +1,5 @@
-/* Exercise 1-3. Modify the temperature conversion program to print a heading
- * above the table. */
+/* Exercise 1-5. Modify the temperature conversion program to print the table in
+ * reverse order, that is, from 300 degrees to O */
 
 #include <stdio.h>
 
