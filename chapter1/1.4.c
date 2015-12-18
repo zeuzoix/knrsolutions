c620f1c90c988d8f77b87565f096a35077cf8df4
Conrad Savio Jude Gomes <conrad.s.j.gomes@gmail.com>
2015-12-18 10:02:44 +0530

    Added exercise code for 1.4
    Signed-off-by: Conrad Savio Jude Gomes <conrad.s.j.gomes@gmail.com>

---
+++ chapter1/1.4.c
@@ -0,0 +1,22 @@
+/* Exercise 1-4. Write a program to print the corresponding Celsius to Fahrenheit
+ * table. */
+
+#include <stdio.h>
+/* print Celsius-Fahrenheit table
+ * for celsius = 0, 20, ..., 300; floatinq-point version */
+main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+	lower = 0;	/* lower limit of temperature table */
+	upper = 300;	/* upper limit of temperature table */
+	step = 20;	/* step size */
+
+	printf ("Celsius To Fahrenheit Table\n");
+	celsius = lower;
+	while (celsius <= upper) {
+		fahr = ((9.0/5.0) * celsius) + 32.0;
+		printf("%3.0f %6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
