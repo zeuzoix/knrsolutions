5cd7f600ff73235f94262e8be047231b7421d461
Conrad Savio Jude Gomes <conrad.s.j.gomes@gmail.com>
2016-03-10 08:54:26 +0530

    Exercise 1-6. Verify that the expression getchar() != EOF is 0 or 1.
    Signed-off-by: Conrad Savio Jude Gomes <conrad.s.j.gomes@gmail.com>

---
+++ chapter1/1.6.c
@@ -0,0 +1,16 @@
+/* Exercise 1-6. Verify that the expression getchar() != EOF is 0 or 1. */
+/* CTRL + D simulates EOF */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+	int result = 0;
+
+	result = (getchar() != EOF); 
+	printf("result = %d\n", result);
+
+	return 0;
+}
