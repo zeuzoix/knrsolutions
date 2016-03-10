f47060f3110b486c3a8f2df9a1b906cfc23314b0
Conrad Savio Jude Gomes <conrad.s.j.gomes@gmail.com>
2016-03-10 08:54:59 +0530

    Exercise 1-7. Write a program to print the value of EOF.
    Signed-off-by: Conrad Savio Jude Gomes <conrad.s.j.gomes@gmail.com>

---
+++ chapter1/1.7.c
@@ -0,0 +1,11 @@
+/* Exercise 1-7. Write a program to print the value of EOF. */
+/* EOF is defined in stdio.h */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	printf("Value of EOF is %d\n", EOF);
+
+	return 0;
+}
